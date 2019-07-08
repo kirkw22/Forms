0af1a428a1a79bb9ad9354823e4f83bcd982c4af
kwoolery42 <kwoolery@liftablemedia.com>
2019-07-08 10:21:06 -0700

    lesson 40

--- src/App.vue
+++ src/App.vue
@@ -2,150 +2,37 @@
     <div>
         <app-header/>
         <div class="container">
-           <form>
-               <div class="row">
-                   <div class="col-xs-12">
-                        <h1>Contact us</h1>
-                        <hr>
+            <div>
+                <button @click="compToRender = 'compHome'">Home </button>
+                <button @click="compToRender = 'compContact'">Contact </button>
+                <button @click="compToRender = 'compPage'">Page </button>
+            </div>
 
-                        <div class="form-group">
-                            <label for="name">Name</label>
-                            <input 
-                                type="text" 
-                                id="name"
-                                class="form-control"
-                                v-model.lazy="formdata.name"
-                            >
-                            {{ formdata.name }}
-                        </div>
-
-                        <div class="form-group">
-                            <label for="lastname">Lastname</label>
-                            <input 
-                                type="text" 
-                                id="lastname"
-                                class="form-control"
-                                v-model="formdata.lastname"
-                            >
-                        </div>
-
-                        <div class="form-group">
-                            <label for="subject">Subject</label>
-                            <input 
-                                type="text" 
-                                id="subject"
-                                class="form-control"
-                                v-model="formdata.subject"
-                            >
-                        </div>
-
-                        <div class="form-group">
-                            <label for="message">Message</label>
-                            <textarea
-                                class="form-control"
-                                rows="3"
-                                id="message"
-                                v-model="formdata.message"
-                            ></textarea>
-                        
-                        </div>
-
-
-                        <div class="form-group">
-                            <label for="newsletter">Newsletter</label>
-                            <input
-                                type="checkbox"
-                                id="newsletter"
-                                value="Newsletter"
-                                v-model="formdata.extras"
-                            >
-                            <label for="promotions">Promotions</label>
-                            <input
-                                type="checkbox"
-                                id="promotions"
-                                value="Promotions"
-                                v-model="formdata.extras"
-                            >                         
-                        </div>
-
-                        <div class="form-group">
-                            <label for="human">
-                                <input
-                                    type="radio"
-                                    id="human"
-                                    value="human"
-                                    v-model="formdata.gender"
-                                > Human
-                            </label>
-                            <label for="alien">
-                                <input
-                                    type="radio"
-                                    id="alien"
-                                    value="alien"
-                                    v-model="formdata.gender"
-                                > Alien
-                            </label>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="country">Country</label>
-                            <select
-                                class="form-control"
-                                id="country"
-                                v-model="formdata.country"
-                            >
-                                <option 
-                                    v-for="(country,index) in countries"
-                                    :key="index"
-                                >
-                                    {{ country }}
-                                </option>
-                            </select>
-                        </div>
-
-                        
-                        <button 
-                            class="btn btn-primary"
-                            @click.prevent="submitForm"
-                        >
-                            Submit
-                        </button>
-
-                   </div>
-               </div>
-           </form>
+            <keep-alive>
+                <component :is="compToRender"></component>
+            </keep-alive>
         </div>
         <app-footer/>
     </div>
 </template>
 
 <script>
+    import compContact from './Components/Contact.vue';
+    import compHome from './Components/Home.vue';
+    import compPage from './Components/Page.vue';
+
     export default {
         data(){
-            return {
-                countries:[
-                    'EEUU',
-                    'India',
-                    'UK',
-                    'Russia'
-                ],
-                formdata:{
-                    name:'',
-                    lastname:'',
-                    subject:'',
-                    message:'Some default content',
-                    extras:[],
-                    gender:'alien',
-                    country:'India'
-                }
-            }
+           return {
+               compToRender:'compContact'
+           }
         },
-        methods:{
-            submitForm(){
-                console.log(this.formdata);
-                console.log(JSON.stringify(this.formdata))
-            }
+        components:{
+            compContact,
+            compHome,
+            compPage
         }
+       
     }
 </script>
 
---
+++ src/Components/Page.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        PAGE
+    </div>
+</template>
+
+<script>
+export default {
+    destroyed(){
+        console.log('destroyed');
+    },
+    activated(){
+        console.log('activated');
+    },
+    deactivated(){
+        console.log('deactivated');
+    }
+}
+</script>
